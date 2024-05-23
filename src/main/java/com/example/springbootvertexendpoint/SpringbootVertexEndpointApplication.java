d6a2c6e9ed9db2c0e569fa8d39c7b55c75be1e42
Mike Nielsen <mmni@protonmail.com>
2024-05-23 22:50:38 +0200

    Initial commit

---
+++ src/main/java/com/example/springbootvertexendpoint/SpringbootVertexEndpointApplication.java
@@ -0,0 +1,13 @@
+package com.example.springbootvertexendpoint;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringbootVertexEndpointApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SpringbootVertexEndpointApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/example/springbootvertexendpoint/SpringbootVertexEndpointApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.springbootvertexendpoint;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringbootVertexEndpointApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
